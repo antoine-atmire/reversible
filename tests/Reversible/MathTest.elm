fc9df179c620a7132aaa59226db86fc566cbcb49
Antoine Snyers <antoine@atmire.com>
2018-08-24 16:59:37 +0200

    Add tests for add and subtract

--- tests/Reversible/MathTest.elm
+++ tests/Reversible/MathTest.elm
@@ -1,13 +1,23 @@
 module Reversible.MathTest exposing (suite)
 
 import Expect
-import Reversible
+import Reversible exposing (..)
+import Reversible.Math as Math
 import Test exposing (Test, describe, test)
 
 
 suite : Test
 suite =
     describe "The Reversible.Math module"
-        [ test "setup" <|
-            \_ -> Expect.equal 1 1
+        [ test "add" <|
+            \_ -> Expect.equal (apply (Math.add 2) 1) (1 + 2)
+        , test "reverse add" <|
+            \_ ->
+                Expect.equal (applyReverse (Math.add 2) 1) (1 - 2)
+        , test "subtract" <|
+            \_ ->
+                Expect.equal (apply (Math.subtract 5) 25) (25 - 5)
+        , test "reverse subtract" <|
+            \_ ->
+                Expect.equal (applyReverse (Math.subtract 5) 25) (25 + 5)
         ]
